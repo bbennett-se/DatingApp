7d41c8ff84c8b233c947e878be1817c8579ea4fe
bbenne13 <bbenne13@students.towson.edu>
2025-01-06 13:12:00 -0800

    started on AuthContext

---
+++ client/src/contexts/AuthContext.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useContext, useState } from 'react'
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+    const [ currentUser, setCurrentUser ] = useState()
+    const value = {
+        currentUser
+    }
+  return (
+    <AuthContext.Provider value = {value}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
