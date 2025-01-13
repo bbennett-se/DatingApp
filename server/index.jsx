a44760151e70299c41b39f04ba0cb7917e49c4aa
bbenne13 <bbenne13@students.towson.edu>
2025-01-13 09:34:58 -0800

    preparing for cloudinary/multer integration

--- server/index.jsx
+++ server/index.jsx
@@ -7,7 +7,13 @@ const cors = require('cors')
 const bcrypt = require('bcrypt')
 require('dotenv').config()
 
+import { v2 as cloudinary } from 'cloudinary'
+
 const uri = process.env.URI
+
+const cloud_name = process.env.CLOUDINARY_CLOUD_NAME
+const api_key = process.env.CLOUDINARY_API_KEY
+const api_secret = process.env.CLOUDINARY_API_SECRET
 
 const app = express()
 app.use(cors())
